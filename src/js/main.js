b5baaa9da84afae0688f87615f2c361e77d6bd7d
Lareta064 <lareta064@gmail.com>
2023-05-09 21:37:55 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (){
+	const bodyEl = document.body;
 	/*======= скрипт, что бы отображить блок с результатми поиска */
 	const headerSearch = document.querySelector('.header-search-form');
 	if(headerSearch){
@@ -153,6 +154,53 @@ document.addEventListener("DOMContentLoaded", function (){
 
 	}
     
+         /* =============== modal с атрибутом frame-modal ===============*/ 
+    const modalFramesOpen = document.querySelectorAll('[frame-btn]');
+    const modalFrames = document.querySelectorAll('[frame-modal]');
+    if( modalFrames.length > 0){
+      const modalFramesClose = document.querySelectorAll('[frame-close]');
+
+      for(let item of modalFramesOpen){
+        item.addEventListener('click', function(e){
+          for(let item of  modalFrames){
+            item.classList.remove('visible');
+            
+            bodyEl.classList.remove('lock');
+          }
+          e.preventDefault();
+          const itemAttr = item.getAttribute('frame-btn');
+
+          for(let frame of modalFrames){
+            const frameAttr =frame.getAttribute('frame-modal');	
+            if(frameAttr == itemAttr){
+              frame.classList.add('visible');
+              bodyEl.classList.add('lock');
+			 
+            }
+          }
+        });
+      }
+      /*==  закрыть модалки  frame-modal по клику на крестик ======*/
+      for(let item of modalFramesClose){
+        item.addEventListener('click', function(e){
+          e.preventDefault();
+          item.closest('[frame-modal]').classList.remove('visible');
+          bodyEl.classList.remove('lock');
+		  
+		  
+        });
+      }
+      /*=============== закрыть модалки по клику вне ===============*/
+      for(let frame of modalFrames){
+        frame.addEventListener('click', function(e){
+          if(e.target === e.currentTarget){
+            this.classList.remove(`visible`);
+            bodyEl.classList.remove('lock');
+          }
+        });
+      }
+    }
+
 	/*======= показать строку поиска на моб версии по клику на иконку */
 	const openSearchMobile = document.querySelector('.header-search-open');
 	if(openSearchMobile){
@@ -164,14 +212,4 @@ document.addEventListener("DOMContentLoaded", function (){
 		
 	}
 
-	const multiLevelMenu = 	document.querySelector('.multiLevel-menu');
-	if(multiLevelMenu){
-		const menuIconItem = multiLevelMenu.querySelectorAll('.level-icon');
-		
-		for(let icon of menuIconItem){
-			console.log(icon);
-			const nextSib = icon.nextElemetSibling;
-			console.log(nextSib);
-		}
-	}
 });
