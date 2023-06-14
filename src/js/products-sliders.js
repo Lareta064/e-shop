e02b5efe275c22583d455fed7d4cc385293bc4c3
Lareta064 <lareta064@gmail.com>
2023-06-14 16:47:37 +0300

    fix-multi-sliders

--- src/js/products-sliders.js
+++ src/js/products-sliders.js
@@ -30,5 +30,23 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		}
 	});
-
+    
+	const columnSliders = $('.column-slider');
+	$(columnSliders).owlCarousel({
+		items:1,
+		nav:true,
+		margin:10,
+		smartSpeed:700,
+		navSpeed:700,
+		navText: [
+			'<svg xmlns="http://www.w3.org/2000/svg" width="5" height="9" fill="none" viewBox="0 0 5 9"><path fill="#000" fill-rule="evenodd" d="M1.935 4.209a.5.5 0 00.004.65l2.439 2.814a.5.5 0 01-.756.655L1.183 5.514a1.5 1.5 0 01-.01-1.952L3.618.677a.5.5 0 11.763.646L1.935 4.21z" clip-rule="evenodd"></path></svg>',
+			'<svg xmlns="http://www.w3.org/2000/svg" width="5" height="9" fill="none" viewBox="0 0 5 9"><path fill="#000" fill-rule="evenodd" d="M3.065 4.791a.5.5 0 00-.004-.65L.622 1.327a.5.5 0 01.756-.655l2.439 2.814a1.5 1.5 0 01.01 1.952L1.382 8.323a.5.5 0 11-.763-.646L3.065 4.79z" clip-rule="evenodd"></path></svg>'],
+		responsive: {
+			600:{items:2},
+			992: { items: 1 },
+			1499: { margin: 20, }
+		}
+			
+	});
+	
 });
